c7175a6a7d1883810cf8dd1625a4ddfad4e699f9
Musab <musab.obaid@gmail.com>
2023-09-17 22:12:17 +0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,6 @@ html_theme_options = {
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': True,
-    'style_nav_header_background': 'white',
     # Toc options
     'collapse_navigation': False,
     'sticky_navigation': True,
